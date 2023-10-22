670647bcc5cb560e52e91e854b8bc942f898dcd7
CESAR CASALE <cesarfcasale@gmail.com>
2023-10-22 16:03:20 +0200

    chronology

---
+++ src/components/reverseTimeline/ReverseTimeline.jsx
@@ -0,0 +1,25 @@
+import "../timeline/Timeline.css";
+
+
+export default function ReverseTimeline({arrayChar}){
+
+    return (
+        <div className="elDiv timeline">
+
+            {arrayChar.filter( (item) => item.age ).sort((a, b) => b.age - a.age).map((item, index) => (
+        <div key={index} className={(index%2===0)? 'container left' : 'container right'}>
+            <div className="card content">
+                <div className="edadNombre">
+                    <h5>{item.age}</h5>
+                    <h4>{item.name}</h4>
+                </div>
+                <div className="divImagenes">
+                    <img className="chronoImg" src={`http://localhost:3000/${item.image}`} alt="Images"/>
+                </div>
+            </div>  
+        </div>  
+            ))}
+        </div>
+    )
+}
+
